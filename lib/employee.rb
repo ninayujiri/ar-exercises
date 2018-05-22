f34e53e0059300c803632032b70b0a47f1830572
Nina Yujiri <ninayujiri@gmail.com>
2018-05-22 18:41:25 +0000

    added Employee class to lib

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,10 @@
 class Employee < ActiveRecord::Base
-
+  belongs_to :store
+  validates_presence_of :first_name, :last_name, :store_id, :hourly_rate
+  validates :hourly_rate,
+      numericality: {
+        only_integer: true,
+        greater_than_or_equal_to: 40,
+        less_than_or_equal_to: 200
+      }
 end
